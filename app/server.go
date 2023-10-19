ddf1eb7ea11171fda974d2846aa65a38805e7c96
Anwar Ziani <anwaar.ziani@gmail.com>
2023-10-19 12:21:13 -0700

    Respond with 200 OK

--- app/server.go
+++ app/server.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		log.Fatalln("Failed to bind to port 4221")
 	}
 
-	fmt.Println("Listening on 4221")
-	_, err = l.Accept()
+	log.Println("Listening on 4221")
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		log.Fatalf("Error accepting connection: %s", err.Error())
+	}
+
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		log.Fatalf("Failed to respond %v", err)
 	}
 }

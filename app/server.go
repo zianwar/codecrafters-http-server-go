201e3fbcc559524fd08f984efa2cada9578bdd3e
Anwar Ziani <anwaar.ziani@gmail.com>
2023-10-19 12:26:06 -0700

    Listen in a loop

--- app/server.go
+++ app/server.go
@@ -14,16 +14,17 @@ func main() {
 
 	log.Println("Listening on 4221")
 
-	conn, err := l.Accept()
-	if err != nil {
-		log.Fatalf("Error accepting connection: %s", err.Error())
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalf("Error accepting connection: %s", err.Error())
+		}
+
+		_, err = fmt.Fprintf(conn, "%s", "HTTP/1.1 200 OK\r\n\r\n")
+		if err != nil {
+			log.Fatalf("Failed to respond %v", err)
+		}
+
+		conn.Close()
 	}
-
-	_, err = fmt.Fprintf(conn, "%s", "HTTP/1.1 200 OK\r\n\r\n")
-	if err != nil {
-		log.Fatalf("Failed to respond %v", err)
-	}
-
-	conn.Close()
-
 }

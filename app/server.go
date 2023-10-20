8ae5ea30922774b9f8520e254a47583a38cfa60a
Anwar Ziani <anwaar.ziani@gmail.com>
2023-10-20 14:59:24 -0700

    Refactor

--- app/server.go
+++ app/server.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
 	"strings"
 )
 
+const (
+	lineTerminator = "\r\n"
+	port           = "4221"
+)
+
+type Request struct {
+	method string
+	path   string
+}
+
+type Response struct {
+	status  string
+	body    string
+	headers map[string]string
+}
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		log.Fatalln("Failed to bind to port 4221")
+		log.Fatalln("Failed to bind to port " + port)
 	}
+	log.Println("Listening on " + port)
 
-	log.Println("Listening on 4221")
-
-	// listen to incoming connections (blocking)
+	// listen to incoming connections
 	conn, err := l.Accept()
 	if err != nil {
 		log.Fatalf("Error accepting connection: %s", err.Error())
@@ -24,40 +40,104 @@ func main() {
 	handleConn(conn)
 }
 
+func parseRequest(conn net.Conn) (*Request, error) {
+	lines := readRequestLines(conn)
+	method, path, ok := parseStatusLineParts(lines[0])
+	if !ok {
+		return nil, fmt.Errorf("failed to parse request status line %s", lines[0])
+	}
+	req := &Request{
+		method: method,
+		path:   path,
+	}
+	return req, nil
+}
+
+func readRequestLines(conn net.Conn) []string {
+	lines := []string{}
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil || line == "\r\n" {
+			break
+		}
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+func parseStatusLineParts(statusLine string) (string, string, bool) {
+	method, rest, ok1 := strings.Cut(statusLine, " ")
+	path, _, ok2 := strings.Cut(rest, " ")
+	if !ok1 || !ok2 {
+		return "", "", false
+	}
+	return method, path, true
+}
+
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 
-	// read data from connection
-	buffer := make([]byte, 1024) // 1024 is a good start
-	_, err := conn.Read(buffer)
+	// Read and parse request
+	req, err := parseRequest(conn)
 	if err != nil {
-		log.Printf("Error reading data: %v", err)
+		log.Printf("failed to parse request: %v\n", err)
+		return
 	}
 
-	_, rest, ok := strings.Cut(string(buffer), " ")
-	if !ok {
-		log.Println("Invalid http request")
-	}
-	path, _, ok := strings.Cut(rest, " ")
-	if !ok {
-		log.Println("Invalid http request")
-	}
-	fmt.Println(path)
-	// send response
-	response := "HTTP/1.1 404 Not Found\r\n\r\n"
-	if path == "/" {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if strings.HasPrefix(strings.ToLower(path), "/echo/") {
-		_, content, _ := strings.Cut(strings.TrimPrefix(path, "/"), "/")
-		response = strings.Join([]string{
-			"HTTP/1.1 200 OK\r\n",
-			"Content-Type: text/plain\r\n",
-			fmt.Sprintf("Content-Length: %d\r\n\r\n", len(content)),
-			content,
-		}, "")
-	}
-	_, err = fmt.Fprintf(conn, "%s", response)
+	// fmt.Println("Request:")
+	// fmt.Printf("%q\n", req)
+
+	// Handle request and build a response
+	resp := handleRequest(req)
+	rawResp := resp.Raw()
+
+	// fmt.Println("Response:")
+	// fmt.Printf("%q\n", rawResp)
+
+	// Write response
+	_, err = conn.Write([]byte(rawResp))
 	if err != nil {
-		log.Fatalf("Failed to respond %v", err)
+		log.Printf("Failed to write response: %v\n", err)
+		return
 	}
+}
+
+func (r *Response) Raw() string {
+	var sb strings.Builder
+
+	sb.WriteString(r.status + lineTerminator)
+	if len(r.headers) > 0 {
+		for k, v := range r.headers {
+			sb.WriteString(fmt.Sprintf("%s: %s", k, v) + lineTerminator)
+		}
+	}
+	sb.WriteString(lineTerminator)
+	if len(r.body) > 0 {
+		sb.Write([]byte(r.body))
+	}
+	return sb.String()
+}
+
+func handleRequest(req *Request) Response {
+	if req.path == "/" {
+		return Response{status: "HTTP/1.1 200 OK"}
+	}
+	if strings.HasPrefix(req.path, "/echo/") {
+		return handleEcho(req.path)
+	}
+	return Response{status: "HTTP/1.1 404 Not Found"}
+}
+
+func handleEcho(path string) Response {
+	path = strings.TrimPrefix(path, "/")
+	_, content, _ := strings.Cut(path, "/")
+
+	return Response{
+		status: "HTTP/1.1 200 OK",
+		body:   content,
+		headers: map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(content)),
+		}}
 }

37a3ddb9b05eafe6d0c55d479bec7be35bb09061
Anwar Ziani <anwaar.ziani@gmail.com>
2023-10-20 12:11:30 -0700

    Read data from conn

--- app/server.go
+++ app/server.go
@@ -14,17 +14,25 @@ func main() {
 
 	log.Println("Listening on 4221")
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatalf("Error accepting connection: %s", err.Error())
-		}
-
-		_, err = fmt.Fprintf(conn, "%s", "HTTP/1.1 200 OK\r\n\r\n")
-		if err != nil {
-			log.Fatalf("Failed to respond %v", err)
-		}
-
-		conn.Close()
+	// listen to incoming connections (blocking)
+	conn, err := l.Accept()
+	if err != nil {
+		log.Fatalf("Error accepting connection: %s", err.Error())
 	}
+
+	// read data from connection
+	buffer := make([]byte, 1024) // 1024 is a good start
+	_, err = conn.Read(buffer)
+	if err != nil {
+		log.Printf("Error reading data: %v", err)
+	}
+
+	// send response
+	_, err = fmt.Fprintf(conn, "%s", "HTTP/1.1 200 OK\r\n\r\n")
+	if err != nil {
+		log.Fatalf("Failed to respond %v", err)
+	}
+
+	conn.Close()
+
 }
